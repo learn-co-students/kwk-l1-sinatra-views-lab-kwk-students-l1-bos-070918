bb19a84221b99138beee05005ca14adf3de869e6
Hope Zouzas <hmzouzas@gmail.com>
2018-07-17 17:45:15 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 class App < Sinatra::Base
+get '/' do 
+  "welcome to our website"
+end 
 
-	get '/' do
-		erb :index
-	end
-
-
+  get '/hello' do
+    erb :hello
+  end
+  
+  get '/goodbye' do 
+    erb :goodbye
+  end 
+  
+  
 end
